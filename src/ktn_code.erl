88502f51d0dcd22fa8d3a024ef6fac1dd0abe914
elbrujohalcon <fernando_benavides_rodriguez@baxter.com>
2025-06-09 13:03:08 +0200

    Format

--- src/ktn_code.erl
+++ src/ktn_code.erl
@@ -18,13 +18,13 @@
 -type tree_node_type() ::
     'case' | 'catch' | 'else' | 'fun' | 'if' | 'maybe' | 'receive' | 'try' | any | atom |
     b_generate | bc | bc_expr | binary | binary_element | block | call | callback |
-    case_clauses | case_expr | char | clause | comment | cons | default | define | else_attr | export |
-    float | function | generate | if_attr | import | integer | lc | lc_expr | m_generate | macro | map |
-    map_field_assoc | map_field_exact | match | maybe_match | mc | mc_expr | module |
-    named_fun | nil | nominal | op | opaque | query | receive_after | receive_case | record |
-    record_attr | record_field | record_index | remote | remote_type | root | spec | string |
-    try_after | try_case | try_catch | tuple | type | type_attr | type_map_field |
-    typed_record_field | user_type | var | atom().
+    case_clauses | case_expr | char | clause | comment | cons | default | define | else_attr |
+    export | float | function | generate | if_attr | import | integer | lc | lc_expr |
+    m_generate | macro | map | map_field_assoc | map_field_exact | match | maybe_match | mc |
+    mc_expr | module | named_fun | nil | nominal | op | opaque | query | receive_after |
+    receive_case | record | record_attr | record_field | record_index | remote | remote_type |
+    root | spec | string | try_after | try_case | try_catch | tuple | type | type_attr |
+    type_map_field | typed_record_field | user_type | var | atom().
 -type tree_node() ::
     #{type => tree_node_type(),
       attrs => map(),
